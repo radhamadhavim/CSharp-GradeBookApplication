9c1bf0f8a9094a92639941e50f214305d14b99bf
radhamadhavim <madhavi.malireddy@gmail.com>
2019-12-25 20:58:40 -0800

    Completed module

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -42,7 +42,7 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            var type = parts[2];
+            var type = parts[2].ToLower();
             BaseGradeBook gradeBook;
             if (type == "standard")
             {
