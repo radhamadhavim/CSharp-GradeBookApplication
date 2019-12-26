dfd86ce0858ed8603575fdd930cc4f32847746ae
radhamadhavim <madhavi.malireddy@gmail.com>
2019-12-25 21:00:34 -0800

    Completed module

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -54,7 +54,7 @@ namespace GradeBook.UserInterfaces
             }
             else
             {
-                Console.WriteLine($"{name} is not a supported type of gradebook, please try again");
+                Console.WriteLine($"{type} is not a supported type of gradebook, please try again");
                 return;
             }   
 
