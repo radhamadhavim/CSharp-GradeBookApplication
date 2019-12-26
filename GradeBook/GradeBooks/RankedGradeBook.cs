184fc74d982f4fa745f7cbe7b767d4a37ea00329
radhamadhavim <madhavi.malireddy@gmail.com>
2019-12-25 20:17:53 -0800

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -19,15 +19,16 @@ namespace GradeBook.GradeBooks
             {
                 throw new InvalidOperationException("Ranked - grading requires a minimum of 5 students to work");
             }
+            
             var index = (int)(Students.Count() * 0.2);
             var grades = Students.OrderByDescending(x => x.AverageGrade).Select(x => x.AverageGrade).ToList();
             if (averageGrade > grades[index])
                 return 'A';
-            else if (averageGrade >= grades[index*2])
+            else if (averageGrade > grades[index*2])
                 return 'B';
-            else if (averageGrade >= grades[index*3])
+            else if (averageGrade > grades[index*3])
                 return 'C';
-            else if (averageGrade >= grades[index*4])
+            else if (averageGrade > grades[index*4])
                 return 'D';
             else
                 return 'F';
